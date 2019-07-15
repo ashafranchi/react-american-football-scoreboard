353d005b1be76de2c80efedf234c471e8d2e8868
Asha <asha17@brown.edu>
2019-07-15 16:38:14 -0700

    stretch styling changes completed

--- src/App.js
+++ src/App.js
@@ -22,14 +22,14 @@ function App() {
         { /* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */ }
 
         <
-        div className = "home__score" > { homeScore } < /div> <
-        /div> <
+        div className = "home__score" > { homeScore } < /div> < /
+        div > <
         div className = "timer" > 00: 03 < /div> <
         div className = "away" >
         <
         h2 className = "away__name" > Tigers < /h2> <
-        div className = "away__score" > { awayScore } < /div> <
-        /div> <
+        div className = "away__score" > { awayScore } < /div> < /
+        div > <
         /div> <
         BottomRow / >
         <
@@ -38,23 +38,27 @@ function App() {
         <
         div className = "homeButtons" > { /* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */ } <
         button onClick = {
-            () => sethomeScore(homeScore + 7) }
+            () => sethomeScore(homeScore + 7)
+        }
         className = "homeButtons__touchdown" > Home Touchdown < /button> <
         button onClick = {
-            () => sethomeScore(homeScore + 3) }
-        className = "homeButtons__fieldGoal" > Home Field Goal < /button> <
-        /div> <
+            () => sethomeScore(homeScore + 3)
+        }
+        className = "homeButtons__fieldGoal" > Home Field Goal < /button> < /
+        div > <
         div className = "awayButtons" >
         <
         button onClick = {
-            () => setawayScore(awayScore + 7) }
+            () => setawayScore(awayScore + 7)
+        }
         className = "awayButtons__touchdown" > Away Touchdown < /button> <
         button onClick = {
-            () => setawayScore(awayScore + 3) }
-        className = "awayButtons__fieldGoal" > Away Field Goal < /button> <
-        /div> <
-        /section> <
-        /div>
+            () => setawayScore(awayScore + 3)
+        }
+        className = "awayButtons__fieldGoal" > Away Field Goal < /button>< /
+        div > <
+        /section> < /
+        div >
     );
 }
 
